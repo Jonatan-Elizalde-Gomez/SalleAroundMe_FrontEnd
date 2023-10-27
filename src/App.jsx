4851e2debe30505efa660ff72ceba75cb4d358c1
IaelChavez <jcs72484@udelasalle.edu.mx>
2023-10-26 19:19:14 -0600

    *Se implementaron rutas

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import RoutesPath from './Routes/Routes';
+
+function App() {
+  return (
+    
+    <RoutesPath>
+    </RoutesPath>
+  );
+}
+
+export default App;
