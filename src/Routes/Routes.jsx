42d31b1580306e82eb195d78aa5242323991de89
Andrés Cordero <andres.cordero@cbqasolutions.com>
2023-10-27 15:03:48 -0600

    Feature: Cards of entities

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from '../Pages/web-app/index';
-import Login from '../Pages/admin-dashboard/Login.jsx';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "../Pages/web-app/index";
+import Login from "../Pages/admin-dashboard/Login.jsx";
 
 const RoutesPath = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<Login/>} />
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/login" element={<Login />} /> */}
+        <Route path="/login" element={<AdminDashboard />} />
       </Routes>
     </Router>
   );
