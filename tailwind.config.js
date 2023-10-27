39ec6c6584890abcb1e3fb4317ca3e02197737fd
Jonatan Elizalde Gómez <jonatanelizaldegomez@gmail.com>
2023-10-27 12:48:27 -0600

    Feature: Layout admin dashboard
    - Se agregó el layout de la página, que incluye el sidebar y la barra superior.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'sw': {
+          'white': '#FFFFFF',
+          'main-darker': '#F8FAFC', // Main White (Darker) - General background
+          'black': '#313131', // Main Black - Text base color
+          'gray-border': '#E3E3E8', // Main Gray (Border)
+          'main-lighter': '#F6F6F9', // Main Gray (Lighter) - Gray background
+          'gray': '#6F6F6F', // Main Gray - Text gray
+          'blue': '#0E8BD1', // Main Blue - Active states
+          'red': '#A51E21', // Main Red - Error states
+          'green': '#29B95F', // Main Green - Success states
+          'yellow': '#FBBF24', // Main Yellow - Warning states
+          'gray-placeholder': '#D6D3D1', // Main Gray (Placeholder)
+        },
+      },
+      fontFamily: {
+        'inter': ['Inter', 'sans'], // Add the Inter font
+      },
+    },
   },
   plugins: [],
 }
-
