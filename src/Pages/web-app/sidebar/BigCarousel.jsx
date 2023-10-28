eb773bd858edc8c0f224e5c987f9c8edf2009a57
Ricardo-CBQA <140541333+Ricardo-CBQA@users.noreply.github.com>
2023-10-27 19:11:48 -0600

    fix_BigCarousel

--- src/Pages/web-app/sidebar/BigCarousel.jsx
+++ src/Pages/web-app/sidebar/BigCarousel.jsx
@@ -1,14 +1,53 @@
-function Card({ id, category, name, description, img }) {
+import Carousel from "nuka-carousel";
+import SwipIcon from "../../../Assets/chevron_icon.svg";
+import CloseIcon from "../../../Assets/close_white.svg";
+
+function BigCarousel({ images, setBigCarrousel }) {
   return (
-    <button className="w-full text-left outline-none flex justify-between items-center px-8 py-4 border-b hover:bg-sw-main-lighter">
-      <div className="w-[70%]">
-        <p className="text-sw-gray text-xs">{category}</p>
-        <p className="text-sw-black font-medium">{name}</p>
-        <p className="text-sw-black text-xs font">{description}</p>
+    <div className="absolute flex items-center justify-center z-50 top-0 right-0 w-full h-screen bg-[rgba(0,0,0,0.7)]">
+      <button
+        className="absolute top-16 right-32"
+        onClick={() => setBigCarrousel(false)}
+      >
+        <img src={CloseIcon} alt="close-icon" />
+      </button>
+      <div className="w-[70%] h-[70vh]">
+        <Carousel
+          wrapAround={true}
+          slidesToShow={1}
+          renderCenterLeftControls={({ previousSlide }) => (
+            <button
+              className="bg-sw-white rounded-full p-4 border absolute -left-[8%] top-[40%]"
+              onClick={previousSlide}
+            >
+              <img className="rotate-90 w-8" src={SwipIcon} alt="before-icon" />
+            </button>
+          )}
+          renderCenterRightControls={({ nextSlide }) => (
+            <button
+              className="bg-sw-white rounded-full p-4 border absolute -right-[8%] top-[40%]"
+              onClick={nextSlide}
+            >
+              <img
+                className="-rotate-90 w-8"
+                src={SwipIcon}
+                alt="before-icon"
+              />
+            </button>
+          )}
+        >
+          {images.map((image) => (
+            <img
+              key={image.image}
+              className="rounded-xl w-full h-[70vh] object-cover"
+              src={image}
+              alt="all-images"
+            />
+          ))}
+        </Carousel>
       </div>
-      <img className="w-24 h-24 rounded-xl" src={img} alt="img-card" />
-    </button>
+    </div>
   );
 }
 
-export default Card;
+export default BigCarousel;
